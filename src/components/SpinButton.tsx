9487fcc7f426ef556bbcf419736320a3bdfa9815
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-28 11:46:56 +0900

    feat: 스크린 리더 적용

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,21 +1,37 @@
-import React, { useState, MouseEvent } from "react";
+import React, { useState, MouseEvent, useEffect } from "react";
 import "./SpinButton.css";
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
+  const [announceText, setAnnounceText] = useState<string>("");
 
   const increment = () => {
+    if (count === 3) return;
     setCount((prevCount) => prevCount + 1);
+    setAnnounceText("성인 1 텍스트 숫자만 수정");
   };
 
   const decrement = () => {
+    if (count === 0) return;
+
     setCount((prevCount) => prevCount - 1);
+    setAnnounceText(`승객추가 ${count}`);
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
     setIsTooltipVisible(!isTooltipVisible);
   };
+
+  useEffect(() => {
+    // announceText가 변경되면 스크린 리더에게 알려줍니다.
+    if (announceText) {
+      const liveRegion = document.getElementById("live-region");
+      if (liveRegion) {
+        liveRegion.innerText = announceText;
+      }
+    }
+  }, [announceText]);
 
   return (
     <section className="spinButtonContainer">
@@ -34,7 +50,11 @@ const SpinButton: React.FC = () => {
             )}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button
+          onClick={decrement}
+          className="spinButton"
+          aria-label="사용자 한명 줄이기 버튼"
+        >
           -
         </button>
         <input
@@ -43,11 +63,21 @@ const SpinButton: React.FC = () => {
           readOnly
           className="spinButtonInput"
           value={count}
+          aria-label="텍스트 숫자만 수정"
         />
-        <button onClick={increment} className="spinButton">
+        <button
+          onClick={increment}
+          className="spinButton"
+          aria-label="사용자 한명 늘리기 버튼"
+        >
           +
         </button>
       </div>
+      <div
+        id="live-region"
+        aria-live="assertive"
+        style={{ position: "absolute", left: "-9999px" }}
+      ></div>
     </section>
   );
 };
